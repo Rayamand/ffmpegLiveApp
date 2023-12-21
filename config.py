c85104dca510cb96fefcb79dee82e757012187c1
AmirabbasGhasemi <rayamand.ir@gmail.com>
2023-12-22 00:14:54 +0330

    first commit

---
+++ config.py
@@ -0,0 +1,14 @@
+hls='http://193.36.85.30/stream/hls/test6.m3u8'
+
+rtmps=[
+    {
+        'url': 'rtmp://rtmp.cdn.asset.aparat.com:443/event',
+        'key': 'ec902b1cbff57ce8621cec631de2442b7?s=0bc8569957924c74',
+        'vertical': False
+    },
+    {
+        'url': 'rtmps://edgetee-upload-waw1-1.xx.fbcdn.net:443/rtmp',
+        'key': '17917529504765662?s_bl=1&s_fbp=ber1-1&s_prp=waw1-1&s_sw=0&s_tids=1&s_vt=ig&a=AbwOvQRF9uFpvVc_',
+        'vertical': True
+    },
+]
